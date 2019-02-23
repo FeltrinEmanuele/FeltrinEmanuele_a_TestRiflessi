6a5be170cda81388de9a573051a3ce3a557b0dd1
Feltrin Emanuele <Emanuelefeltrin19971010@gmail.com>
2019-02-23 09:53:04 +0100

    fatto

--- FeltrinEmanuele_a_TestRiflessi/FeltrinEmanuele_a_TestRiflessi.ino
+++ FeltrinEmanuele_a_TestRiflessi/FeltrinEmanuele_a_TestRiflessi.ino
@@ -1,9 +1,25 @@
+
+#define LED 13
+#define BUTTON 8
+int val = 0;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  val = digitalRead(BUTTON);
+
+  if (val == HIGH)
+  {
+    digitalWrite(LED,HIGH);
+  }
+  else{
+    digitalWrite(LED,LOW);
+  }
 
 }

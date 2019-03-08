917b4109fde2d85ffea642c29ff9c514a48f655d
FeltrinEmanuele <emanuelefeltrin19971010@gmail.com>
2019-03-08 20:46:07 +0100

    Quasi Fatto (debuggato)

--- FeltrinEmanuele_a_TestRiflessi/FeltrinEmanuele_a_TestRiflessi.ino
+++ FeltrinEmanuele_a_TestRiflessi/FeltrinEmanuele_a_TestRiflessi.ino
@@ -21,15 +21,15 @@ void setup() {
 }
 void loop() {
   // Vedo Se il pulsante è premuto
-  bottoneHIGH_o_LOW = digitalRead(bottone)
+  bottoneHIGH_o_LOW = digitalRead(bottone);
   bool premuto = false;
   while(!premuto){
     if(bottoneHIGH_o_LOW == HIGH){
       premuto = true;
     }
   }
-  risultatoRiflessiLed = ("Il tuo tempo di risposta col led è", ledDiPausa)
-  risultatoRiflessiBuzzer = ("Il tuo tempo di risposta col buzzer è", buzzer)
+  risultatoRiflessiLed = ("Il tuo tempo di risposta col led è", ledDiPausa);
+  risultatoRiflessiBuzzer = ("Il tuo tempo di risposta col buzzer è", buzzer);
                
 }
 
@@ -40,7 +40,7 @@ int ProvaRiflessi(String testo, int led_o_buzzer)
   delay(random(1000,5000));
   digitalWrite(led_o_buzzer,HIGH);
   int TempoInizioProva = millis();
-  bottoneHIGH_o_LOW = digitalRead(bottone)
+  bottoneHIGH_o_LOW = digitalRead(bottone);
   bool premuto = false;
   while(!premuto){
     if(bottoneHIGH_o_LOW == HIGH){
@@ -50,6 +50,6 @@ int ProvaRiflessi(String testo, int led_o_buzzer)
   digitalWrite(led_o_buzzer,LOW);
   int TempoFineProva = millis();
   int TempoFinale = TempoFineProva - TempoInizioProva;
-  Serial.println(messaggio + String (TempoFinale));
+  Serial.println(testo + String (TempoFinale));
   return TempoFinale;  
 }

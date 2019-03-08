97ef19e15741bbad28c09113922c6b91265dc992
FeltrinEmanuele <emanuelefeltrin19971010@gmail.com>
2019-03-08 20:20:40 +0100

    Setup

--- FeltrinEmanuele_a_TestRiflessi/FeltrinEmanuele_a_TestRiflessi.ino
+++ FeltrinEmanuele_a_TestRiflessi/FeltrinEmanuele_a_TestRiflessi.ino
@@ -1,24 +1,23 @@
-#define LED 13
-#define BUTTON 8
-int val = 0;
-
+int ledDiPausa;
+int buzzer;
+int bottone;
+int ledVerde;
+int ledRosso;
+int risultatoRiflessiLed;
+int risultatoRiflessiBuzzer;
+int bottoneHIGH_o_LOW;
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED, OUTPUT);
-  pinMode(BUTTON, INPUT);
-
+  ledVerde = 6;
+  ledRosso = 7;
+  ledDiPausa = 8;
+  buzzer = 9;
+  bottone = 10;
+  pinMode(ledDiPausa,OUTPUT);
+  pinMode(buzzer,OUTPUT);
+  pinMode(bottone,INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  val = digitalRead(BUTTON);
 
-  if (val == HIGH)
-  {
-    digitalWrite(LED,HIGH);
-  }
-  else{
-    digitalWrite(LED,LOW);
-  }
-     //fai il codice delk buzzer qua              
+               
 }
